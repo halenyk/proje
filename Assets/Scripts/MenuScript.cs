15ec8ea55eaa49d23da5dc29c737ac9d03ae398b
halenyk <halenykk@gmail.com>
2024-01-15 18:44:31 +0300

    menulu

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+//menu
 public class MenuScript : MonoBehaviour
 {
     public void StartButton()
